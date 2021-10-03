3d59876f4d9d551ec221b656672ad685389be81f
Vitaliy Ruban <vitaliy.ruban@itera.no>
2021-10-03 21:33:02 +0300

    Added empty page for the app plan tests

---
+++ AzurePerfTest/Pages/Empty.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzurePerfTest.Pages
+{
+    public class EmptyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
